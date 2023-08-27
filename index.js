f421a81448acf09e4a1e12062410af0c53256768
thes24 <seungwoo624@gmail.com>
2023-08-27 23:52:27 +0900

    Feat: add HTML pages

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/api/abc', (req, res) => {
+    res.sendFile(__dirname + "/hrsc/401.html")
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
