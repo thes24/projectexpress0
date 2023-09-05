b9acfd10aff7679415d37cdff38801346df23d2c
thes24 <seungwoo624@gmail.com>
2023-09-05 10:59:08 +0900

    Feat: move board password check to Back

--- src/js/boardupdate.js
+++ src/js/boardupdate.js
@@ -28,43 +28,34 @@ const main = () => {
 
     const uptButton = document.getElementById('update');
     uptButton.addEventListener('click', async function() {
-        const enteredPassword = prompt('Enter the board password:');
+        const updatedTitle = document.querySelector('#board-title').value;
+        const updatedPassword = document.querySelector('#board-password').value;
+        const updatedContent = document.querySelector('#board-content').value;
+        const boardWriter = await myjs.getMemberName();
 
-        const response = await swaxios.get(`/api/board/find-id/${boardId}`);
-        const bdata = response.data;
-
-        if (enteredPassword === bdata.boardPassword) {
-
-            const updatedTitle = document.querySelector('#board-title').value;
-            let updatedPassword = document.querySelector('#board-password').value;
-            if (updatedPassword == '') {
-                updatedPassword = bdata.boardPassword;
-            }
-            const updatedContent = document.querySelector('#board-content').value;
-            const boardWriter = await myjs.getMemberName();
-
-            await swaxios.put(`/api/board/update/${boardId}`, {
-                boardId: boardId,
-                boardWriter,
-                boardPassword: updatedPassword,
-                boardTitle: updatedTitle,
-                boardContent: updatedContent,
-            })
-            .then(function (res) {
+        await swaxios.put(`/api/board/update/${boardId}`, {
+            boardId: boardId,
+            boardWriter,
+            boardPassword: updatedPassword,
+            boardTitle: updatedTitle,
+            boardContent: updatedContent,
+        })
+        .then(function (res) {
+            if (res.status == 200) {
                 console.log('Board updated succesfully', res);
                 return res.data;
-            })
-            .then(function (data) {
-                console.log('data', data);
-                window.location.href = '/board';
-            })
-            .catch(function (err) {
-                console.error('error', err);
-            });
-        } else {
-            alert('Incorrect password. Update denied.');
+            } else {
+                console.error('Board update denied', err);
+                alert('Incorrect password. Please try again.');
+            }
+        })
+        .then(function (data) {
+            console.log('data', data);
             window.location.href = '/board';
-        }
+        })
+        .catch(function (err) {
+            console.error('error', err);
+        });
     })
 };
 
